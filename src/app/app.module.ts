6ec11fc400c3517c38bc6b2d7981a5b63a8c44e9
mlienhart <martin.lienhart@outlook.com>
2022-09-04 15:16:32 +0200

    21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ACreateComponent } from './a-create/a-create.component';
-import { BHotVsColdComponent } from './b-hot-vs-cold/b-hot-vs-cold.component';
-import { CSubjectsComponent } from './c-subjects/c-subjects.component';
-import { DOperatorsComponent } from './d-operators/d-operators.component';
-import { ETapComponent } from './e-tap/e-tap.component';
-import { FBackpressureComponent } from './f-backpressure/f-backpressure.component';
-import { GSwitchMapComponent } from './g-switch-map/g-switch-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ACreateComponent,
-    BHotVsColdComponent,
-    CSubjectsComponent,
-    DOperatorsComponent,
-    ETapComponent,
-    FBackpressureComponent,
-    GSwitchMapComponent,
   ],
   imports: [
     BrowserModule
