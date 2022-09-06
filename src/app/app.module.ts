4e2b069a2a25ceab6a57e38e9fd829f8824febea
mlienhart <martin.lienhart@outlook.com>
2022-09-06 13:23:19 +0200

    23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { OverviewComponent } from './overview/overview.component';
+import { DurumComponent } from './durum/durum.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OverviewComponent,
+    DurumComponent,
   ],
   imports: [
     BrowserModule
