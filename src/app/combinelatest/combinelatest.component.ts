9c6eba6a26878448e09f175da6c656cf90c42910
mlienhart <martin.lienhart@outlook.com>
2022-10-29 16:50:20 +0200

    44

--- src/app/combinelatest/combinelatest.component.ts
+++ src/app/combinelatest/combinelatest.component.ts
@@ -14,39 +14,39 @@ export class CombinelatestComponent implements OnInit {
   ngOnInit(): void { }
 }
 
-// const firstList$: Observable<OriginalItem[]> = of([
-//   { id: 4, name: "four" }
-// ]);
-// const secondList$: Observable<OriginalItem[]> = of([
-//   { id: 3, name: "three" },
-//   { id: 5, name: "five" },
-//   { id: 1, name: "one" }
-// ]);
-// const thirdList$: Observable<OriginalItem[]> = of([
-//   { id: 6, name: "six" },
-//   { id: 2, name: "two" },
-// ]);
+const firstList$: Observable<OriginalItem[]> = of([
+  { id: 4, name: "four" }
+]);
+const secondList$: Observable<OriginalItem[]> = of([
+  { id: 3, name: "three" },
+  { id: 5, name: "five" },
+  { id: 1, name: "one" }
+]);
+const thirdList$: Observable<OriginalItem[]> = of([
+  { id: 6, name: "six" },
+  { id: 2, name: "two" },
+]);
 
-// const mappedList$: Observable<MappedItem[]> = firstList$
-//   .pipe(
-//     filter((data) => data.length > 0),
-//     switchMap((first) =>
-//       combineLatest([
-//         secondList$.pipe(
-//           map((data) => data.filter((x) => x.id < first[0].id))
-//         ),
-//         thirdList$
-//       ])
-//     ),
-//     tap(x => console.log(x)),
-//     map(([second, third]) =>
-//       [...second, ...third]),
-//     tap(x => console.log(x)),
-//     map((data) =>
-//       data
-//         .map((x) => <MappedItem>({ code: x.id, label: x.name }))
-//         .sort((a, b) => a.code - b.code)
-//     )
-//   );
+const mappedList$: Observable<MappedItem[]> = firstList$
+  .pipe(
+    filter((data) => data.length > 0),
+    switchMap((first) =>
+      combineLatest([
+        secondList$.pipe(
+          map((data) => data.filter((x) => x.id < first[0].id))
+        ),
+        thirdList$
+      ])
+    ),
+    tap(x => console.log(x)),
+    map(([second, third]) =>
+      [...second, ...third]),
+    tap(x => console.log(x)),
+    map((data) =>
+      data
+        .map((x) => <MappedItem>({ code: x.id, label: x.name }))
+        .sort((a, b) => a.code - b.code)
+    )
+  );
 
-// mappedList$.subscribe(x => console.log(x));
+mappedList$.subscribe(x => console.log(x));
--- src/app/concatmap/concatmap.component.ts
+++ src/app/concatmap/concatmap.component.ts
@@ -15,27 +15,27 @@ export class ConcatmapComponent implements OnInit {
 
 }
 
-const firstList$: Observable<OriginalItem[]> = of([
-  { id: 4, name: "four" }
-]);
-const secondList$: Observable<OriginalItem[]> = of([
-  { id: 3, name: "three" },
-  { id: 5, name: "five" },
-  { id: 1, name: "one" }
-]);
+// const firstList$: Observable<OriginalItem[]> = of([
+//   { id: 4, name: "four" }
+// ]);
+// const secondList$: Observable<OriginalItem[]> = of([
+//   { id: 3, name: "three" },
+//   { id: 5, name: "five" },
+//   { id: 1, name: "one" }
+// ]);
 
-const mappedList$: Observable<MappedItem[]> = firstList$
-  .pipe(
-    concatMap((first) =>
-      secondList$.pipe(
-        map((data) => data.filter((x) => x.id < first[0].id))
-      )
-    ),
-    map((data) =>
-      data
-        .map((x) => <MappedItem>({ code: x.id, label: x.name }))
-        .sort((a, b) => a.code - b.code)
-    )
-  );
+// const mappedList$: Observable<MappedItem[]> = firstList$
+//   .pipe(
+//     concatMap((first) =>
+//       secondList$.pipe(
+//         map((data) => data.filter((x) => x.id < first[0].id))
+//       )
+//     ),
+//     map((data) =>
+//       data
+//         .map((x) => <MappedItem>({ code: x.id, label: x.name }))
+//         .sort((a, b) => a.code - b.code)
+//     )
+//   );
 
-mappedList$.subscribe(x => console.log(x));
+// mappedList$.subscribe(x => console.log(x));
